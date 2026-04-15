07f5afc2116b9be78d3b15d02bc26d6609965883
Ak Bitu <c00313310@setu.ie>
2026-04-15 01:31:21 +0100

    Added guide lists

--- src/routes/guides/+page.svelte
+++ src/routes/guides/+page.svelte
@@ -1,9 +1,32 @@
 <script>
+let guides = [
+        {
+            title: "Altamont Gardens",
+            description: "Beautiful gardens with revers and walking paths."
+        },
+        {
+            title: "Brownshill Dolmen",
+            description: "One of the largest dolmens in Ireland."
+        },
+        {
+            title: "Carlow Castle",
+            description: "A historic castle in Carlow town"
+        }
+        
+    ];
 </script>
 <h1>
     Travel Guides
 </h1>
 <p>Find travel ideas and explore different destinations easily.</p>
+
+{#each guides as guide}
+  <div>
+    <h3>{guide.title}</h3>
+    <p>{guide.description}</p>
+
+  </div>
+  {/each}
 
 <style>
   h1 {
