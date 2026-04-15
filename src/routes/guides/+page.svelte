ccd805a8145cbe17665c982c21fb9adc29815183
Ak Bitu <c00313310@setu.ie>
2026-04-15 01:12:44 +0100

    Add Layout

--- src/routes/guides/+page.svelte
+++ src/routes/guides/+page.svelte
@@ -3,4 +3,17 @@
 <h1>
     Travel Guides
 </h1>
-<p> Start the guide page </p>
+<p>Find travel ideas and explore different destinations easily.</p>
+
+<style>
+  h1 {
+    font-size: 28px;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 16px;
+    color: #444;
+    margin-bottom: 20px;
+  }
+</style>
